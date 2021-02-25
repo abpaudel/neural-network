9af0f301d76a8d45b61f66b89580a118d39f950b
Abhishek Paudel <abpaudel@users.noreply.github.com>
2021-02-24 19:02:53 -0500

    update loss to sum over batches

--- loss.py
+++ loss.py
@@ -3,16 +3,16 @@ import numpy as np
 def mse(y_true, y_pred, derv=False):
     if derv:
         return 2 * (y_pred - y_true) / y_pred.size
-    return np.mean(np.power(y_true - y_pred, 2))
+    return np.mean(np.power(y_true - y_pred, 2), axis=1).sum()
 
 def sse(y_true, y_pred, derv=False):
     if derv:
         return y_pred - y_true
-    return 0.5 * np.sum(np.power(y_true - y_pred, 2))
+    return np.sum(0.5 * np.sum(np.power(y_true - y_pred, 2), axis=1))
 
 def cross_entropy(y_true, y_pred, derv=False):
     # assumes y_pred is output from softmax layer
     if derv:
       return y_pred - y_true
-    return np.mean(-y_true * np.nan_to_num(np.log(y_pred)))
+    return np.mean(-y_true * np.nan_to_num(np.log(y_pred)), axis=1).sum()
     
